bc0be76ab3f9d758dc0c0798c16c90e0746d6077
antitank19 <trankhaiminhkhoi10a3@gmail.com>
2023-03-01 21:05:45 +0700

    Init test

---
+++ API/Controllers/AAAAADateTimeConvertController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AAAAADateTimeConvertController : ControllerBase
+    {
+
+        // POST api/<AAAAADateTimeConvertController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string dateString)
+        {
+            try
+            {
+                //  2009-05-08 14:40:52,531
+                //  5/8/2009 14:40:52
+                //  yyyy-MM-dd HH:mm:ss,fff
+                string[] formats = { "d/M/yyyy HH:mm:ss", "d/M/yyyy" };
+                DateTime myDate = DateTime.ParseExact(dateString, formats,
+                                System.Globalization.CultureInfo.InvariantCulture);
+                return Ok(new { Class= myDate.GetType() ,Value=myDate.ToString("d MMM yyyy HH:mm:ss") });
+            }   
+            catch(Exception ex)
+            {
+                  return BadRequest(ex.Message);
+            }
+        }
+    }
+}
